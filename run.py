0224259dd47a86233e62c940e39f9fc99623a09a
TanishaS133 <tanishasanthosh74@gmail.com>
2026-07-07 13:46:57 +0530

    Add main function with argument parsing

--- run.py
+++ run.py
@@ -8,3 +8,15 @@ import time
 import numpy as np
 import pandas as pd
 import yaml
+def main():
+
+    parser = argparse.ArgumentParser(
+        description="Minimal MLOps Batch Job"
+    )
+
+    parser.add_argument("--input", required=True)
+    parser.add_argument("--config", required=True)
+    parser.add_argument("--output", required=True)
+    parser.add_argument("--log-file", required=True)
+
+    args = parser.parse_args()
